8b31a90124a131ac4797463181bcce6dd21fd8b3
Lantz <lantzshaw@163.com>
2024-09-23 13:33:22 +0800

    feat: add EasyExcelTest

---
+++ src/main/java/com/litecase/boot/web/util/FileUtil.java
@@ -0,0 +1,9 @@
+package com.litecase.boot.web.util;
+
+public class FileUtil {
+    public static String getPath() {
+        String path = FileUtil.class.getClassLoader().getResource("").getPath();
+
+        return path;
+    }
+}
